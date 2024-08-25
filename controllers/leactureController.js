883b8d8c5894e46ee9107f23982628e3be91fcd7
Ahmed Sabry <112628761+SABRY225@users.noreply.github.com>
2024-08-25 14:11:17 +0300

    add route

--- controllers/leactureController.js
+++ controllers/leactureController.js
@@ -82,7 +82,7 @@ const getLecturesByTeacher = async (req, res) => {
         });
         
         // Respond with the lectures along with course details
-        res.status(200).json(courseDetails);
+        res.status(200).json(result);
     } catch (error) {
         res.status(500).json({ message: 'Server error', error });
     }
